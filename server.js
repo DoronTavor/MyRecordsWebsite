71ecb6bec21e51345540bb998a627f2a625c8c96
DoronTavor <tavor.doron@gmail.com>
2023-11-07 14:00:27 +0200

    new updates-connecting to frontend

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ async function run() {
     }
 }
 app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 
 });
 
