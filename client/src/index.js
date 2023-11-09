7e3c703759bf8f16501bf6c534075bc83add159d
DoronTavor <tavor.doron@gmail.com>
2023-11-09 09:43:07 +0200

    new updates-Setting the buttons for the section "Types of records"

---
+++ client/src/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import App from "./App";
+import Modal from "react-modal";
+import {
+    createBrowserRouter,
+        RouterProvider,
+} from "react-router-dom";
+import DetailsForItem from "./pages/DetailsForItem";
+import Login from "./pages/Login";
+import AllCds from "./pages/AllCds";
+import AllVinyls from "./pages/AllVinyls";
+import AddingVinyl from "./pages/AddingVinyl";
+Modal.setAppElement("#root");
+const router= createBrowserRouter([{
+    path:"/",
+    element:<App/>
+},
+    {
+        path:"/Details/:id",
+        element:<DetailsForItem/>
+    },
+    {
+        path:"/login",
+        element:<Login/>
+    },
+    {
+        path:"/allCds",
+        element:<AllCds/>
+    },
+    {
+        path:"/allVinyls",
+        element:<AllVinyls/>
+    },
+    {
+        path:"/AddVinyl",
+        element:<AddingVinyl/>
+    }
+
+]);
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<RouterProvider router={router}/>);
